736db205026ec4203c5d1664f712fb4d2af4bb72
David Cantu <davidcantu966@gmail.com>
2025-08-07 17:57:59 -0600

    refactor: standardize import statements and clean up schema definition

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,8 +1,8 @@
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
+import { drizzle } from 'drizzle-orm/neon-http'
+import { neon } from '@neondatabase/serverless'
 
-import { env } from "~/env";
-import * as schema from "./schema";
+import { env } from '~/env'
+import * as schema from './schema'
 
-const sql = neon(env.DATABASE_URL);
-export const db = drizzle({ client: sql, schema });
+const sql = neon(env.DATABASE_URL)
+export const db = drizzle({ client: sql, schema })
