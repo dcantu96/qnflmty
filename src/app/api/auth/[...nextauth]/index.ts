ff348aaec9012801b09dd1b127e47361580e48de
David Cantu <davidcantu966@gmail.com>
2025-08-10 22:16:37 -0600

    refactor: implement Google sign-in logic with user creation in NextAuth callbacks

--- src/app/api/auth/[...nextauth]/index.ts
+++ src/app/api/auth/[...nextauth]/index.ts
@@ -3,6 +3,8 @@ import GoogleProvider from 'next-auth/providers/google'
 import { DrizzleAdapter } from '@auth/drizzle-adapter'
 import { env } from '~/env'
 import { db } from '~/server/db'
+import { eq } from 'drizzle-orm'
+import { users } from '~/server/db/schema'
 
 export const authOptions = {
 	adapter: DrizzleAdapter(db),
@@ -12,6 +14,31 @@ export const authOptions = {
 			clientSecret: env.GOOGLE_CLIENT_SECRET,
 		}),
 	],
+	callbacks: {
+		async signIn({ user, account, profile }) {
+			if (account?.provider === 'google') {
+				const email = user.email
+				if (!email) {
+					throw new Error('Email is required for Google sign-in')
+				}
+				const existingUser = await db.query.users.findFirst({
+					where: (u, { eq }) => eq(u.email, email),
+				})
+
+				if (!existingUser) {
+					await db
+						.update(users)
+						.set({
+							name: user.name ?? '',
+							image: user.image ?? '',
+						})
+						.where(eq(users.id, user.id))
+				}
+			}
+
+			return true
+		},
+	},
 	session: {
 		strategy: 'database',
 	},
