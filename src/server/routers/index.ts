8a315653457a3af6b8c37d73b9fd6b21ac645a5f
David Cantu <davidcantu966@gmail.com>
2025-08-16 15:05:35 -0600

    feat: implement user management functionality with TRPC integration

--- src/server/routers/index.ts
+++ src/server/routers/index.ts
@@ -1,8 +1,10 @@
 import { createTRPCRouter } from '~/server/trpc'
 import { cypressLoginRouter } from './cypress-login'
+import { usersRouter } from './users'
 
 export const appRouter = createTRPCRouter({
 	cypressLogin: cypressLoginRouter,
+	users: usersRouter,
 })
 
 export type AppRouter = typeof appRouter
