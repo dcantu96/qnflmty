105262afa3021f4490fbb1d745b15b85a4a7acc9
David Cantu <davidcantu966@gmail.com>
2025-08-16 11:58:21 -0600

    feat: add Cypress workflow for automated testing

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+	projectId: 'wxjaty',
 	e2e: {
 		baseUrl: 'http://localhost:3000',
 		specPattern: ['cypress/e2e/**/*.cy.{js,ts}', 'app/**/*.test.ts'],
