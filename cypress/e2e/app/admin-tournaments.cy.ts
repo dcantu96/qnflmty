dcc7a9ba00d4b40a165190696204d0c84e63778c
David Cantu <davidcantu966@gmail.com>
2025-08-28 16:55:03 -0600

    feat: enhance admin tournament view tests with sport and tournament creation

--- cypress/e2e/app/admin-tournaments.cy.ts
+++ cypress/e2e/app/admin-tournaments.cy.ts
@@ -11,6 +11,15 @@ const testAdmin = {
 	email: 'john@wick.com',
 }
 
+const sport = {
+	name: 'American Football',
+}
+
+const tournament = {
+	name: 'National Football League',
+	year: 2025,
+}
+
 describe('An Admin', () => {
 	before(() => {
 		cy.task('createAdmin', testAdmin)
@@ -25,13 +34,42 @@ describe('An Admin', () => {
 		cy.login(testAdmin.email)
 	})
 
-	it('Can view all tournaments', () => {
-		cy.visit('/admin')
-		cy.contains(/data/i).click()
-		cy.contains(/tournaments/i).click()
-		cy.url().should('include', '/admin/tournaments')
-		cy.contains(/tournaments/i)
-		cy.contains(/national football league/i)
-		cy.contains(/joinable/i)
+	describe('Viewing tournaments', () => {
+		it('Should display no results if no tournaments exist', () => {
+			cy.visit('/admin')
+			cy.contains(/data/i).click()
+			cy.contains(/tournaments/i).click()
+			cy.contains(/name/i)
+			cy.contains(/year/i)
+			cy.contains(/sport/i)
+			cy.contains(/no results/i)
+		})
+
+		describe('With one tournament', () => {
+			before(() => {
+				cy.task('createSport', sport).then(({ id }) => {
+					cy.task('createTournament', {
+						...tournament,
+						sportId: id,
+					})
+				})
+			})
+
+			after(() => {
+				cy.task('deleteTournament', { name: tournament.name })
+				cy.task('deleteSport', { name: sport.name })
+			})
+
+			it('Can view tournaments', () => {
+				cy.visit('/admin')
+				cy.contains(/data/i).click()
+				cy.contains(/tournaments/i).click()
+				cy.url().should('include', '/admin/tournaments')
+				cy.contains(/tournaments/i)
+				cy.contains(tournament.name)
+				cy.contains(tournament.year)
+				cy.contains(sport.name)
+			})
+		})
 	})
 })
