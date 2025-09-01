700012bde3982e87bc637fd73bdbaa40d49acb2a
David Cantu <davidcantu966@gmail.com>
2025-09-01 12:32:45 -0600

    feat: update navigation component to use Link for sub-items

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { ChevronRight, type LucideIcon } from 'lucide-react'
+import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { useCallback } from 'react'
 
@@ -58,40 +59,7 @@ export function NavMain({
 				{isAdminRoute ? 'Admin Dashboard' : 'Platform'}
 			</SidebarGroupLabel>
 			<SidebarMenu>
-				{items.map((item) => (
-					<Collapsible
-						key={item.title}
-						asChild
-						defaultOpen={isAnySubItemActive(item.items)}
-						className="group/collapsible"
-					>
-						<SidebarMenuItem>
-							<CollapsibleTrigger asChild>
-								<SidebarMenuButton tooltip={item.title}>
-									{item.icon && <item.icon />}
-									<span>{item.title}</span>
-									<ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
-								</SidebarMenuButton>
-							</CollapsibleTrigger>
-							<CollapsibleContent>
-								<SidebarMenuSub>
-									{item.items?.map((subItem) => (
-										<SidebarMenuSubItem key={subItem.title}>
-											<SidebarMenuSubButton
-												asChild
-												isActive={pathname === subItem.url}
-											>
-												<a href={subItem.url}>
-													<span>{subItem.title}</span>
-												</a>
-											</SidebarMenuSubButton>
-										</SidebarMenuSubItem>
-									))}
-								</SidebarMenuSub>
-							</CollapsibleContent>
-						</SidebarMenuItem>
-					</Collapsible>
-				))}
+													<Link href={subItem.url}>{subItem.title}</Link>
 			</SidebarMenu>
 		</SidebarGroup>
 	)
