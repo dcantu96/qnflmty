01e27525ed09ef2de8d604257486cb07e77c9b2a
David Cantu <davidcantu966@gmail.com>
2025-09-02 12:46:13 -0600

    feat: implement user filtering in admin user view with toggle options

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,12 +1,47 @@
-import { DataTable } from '~/components/ui/data-table/data-table'
 import { columns } from './columns'
 import { getUsers } from '~/server/admin/queries'
+import { DataTable } from '~/components/ui/data-table/data-table'
+import { ToggleGroup, ToggleGroupItem } from '~/components/ui/toggle-group'
+import Link from 'next/link'
 
-export default async function Page() {
-	const { items } = await getUsers()
+type UserSearchParams = {
+	/**
+	 * if no kind is specified, only active users will be returned
+	 */
+	kind?: 'all' | 'suspended'
+}
+
+export default async function Page({
+	searchParams,
+}: {
+	searchParams: Promise<UserSearchParams>
+}) {
+	const filters = await searchParams
+	const { items } = await getUsers({ limit: 1000, page: 1, kind: filters.kind })
 
 	return (
 		<div className="container mx-auto">
+			<ToggleGroup type="single" size="sm" value={filters.kind ?? 'none'}>
+				<ToggleGroupItem className="w-24" value="all" variant="outline" asChild>
+					<Link href="?kind=all">All</Link>
+				</ToggleGroupItem>
+				<ToggleGroupItem
+					className="w-24"
+					value="none"
+					variant="outline"
+					asChild
+				>
+					<Link href="">Active</Link>
+				</ToggleGroupItem>
+				<ToggleGroupItem
+					className="w-24"
+					value="suspended"
+					variant="outline"
+					asChild
+				>
+					<Link href="?kind=suspended">Suspended</Link>
+				</ToggleGroupItem>
+			</ToggleGroup>
 			<DataTable columns={columns} data={items} schema="users" label="Users" />
 		</div>
 	)
