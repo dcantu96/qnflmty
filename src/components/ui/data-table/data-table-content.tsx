0c9adeb4a5bd06f37e10dcd54828108116e1b046
David Cantu <davidcantu966@gmail.com>
2025-09-03 18:15:32 -0600

    feat: implement DataTable components with context for state management

---
+++ src/components/ui/data-table/data-table-content.tsx
@@ -0,0 +1,62 @@
+'use client'
+
+import { flexRender } from '@tanstack/react-table'
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from '../table'
+import { useDataTable } from './use-data-table'
+
+export default function DataTableContent<TData>() {
+	const { table, columnsLength } = useDataTable<TData>()
+	return (
+		<div className="overflow-hidden rounded-md border">
+			<Table>
+				<TableHeader>
+					{table.getHeaderGroups().map((headerGroup) => (
+						<TableRow key={headerGroup.id}>
+							{headerGroup.headers.map((header) => {
+								return (
+									<TableHead key={header.id}>
+										{header.isPlaceholder
+											? null
+											: flexRender(
+													header.column.columnDef.header,
+													header.getContext(),
+												)}
+									</TableHead>
+								)
+							})}
+						</TableRow>
+					))}
+				</TableHeader>
+				<TableBody>
+					{table.getRowModel().rows?.length ? (
+						table.getRowModel().rows.map((row) => (
+							<TableRow
+								key={row.id}
+								data-state={row.getIsSelected() && 'selected'}
+							>
+								{row.getVisibleCells().map((cell) => (
+									<TableCell key={cell.id}>
+										{flexRender(cell.column.columnDef.cell, cell.getContext())}
+									</TableCell>
+								))}
+							</TableRow>
+						))
+					) : (
+						<TableRow>
+							<TableCell colSpan={columnsLength} className="h-24 text-center">
+								No results.
+							</TableCell>
+						</TableRow>
+					)}
+				</TableBody>
+			</Table>
+		</div>
+	)
+}
--- src/components/ui/data-table/data-table-pagination.tsx
+++ src/components/ui/data-table/data-table-pagination.tsx
@@ -1,4 +1,5 @@
-import type { Table } from '@tanstack/react-table'
+'use client'
+
 import {
 	ChevronLeft,
 	ChevronRight,
@@ -14,16 +15,12 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from '../select'
+import { useDataTable } from './use-data-table'
 
-interface DataTablePaginationProps<TData> {
-	table: Table<TData>
-}
-
-export function DataTablePagination<TData>({
-	table,
-}: DataTablePaginationProps<TData>) {
+export default function DataTablePagination<TData>() {
+	const { table } = useDataTable<TData>()
 	return (
-		<div className="flex items-center justify-between px-2">
+		<div className="flex items-center justify-between px-2 py-4">
 			<div className="flex-1 text-muted-foreground text-sm">
 				{table.getFilteredSelectedRowModel().rows.length} of{' '}
 				{table.getFilteredRowModel().rows.length} row(s) selected.
